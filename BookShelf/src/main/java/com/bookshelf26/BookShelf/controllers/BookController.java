fa4a7a81819f97c284abba7d716a23493e9979bf
k1nesi <kolatimakov342@gmail.com>
2024-05-31 20:29:58 +0300

    Add files via upload

--- BookShelf/src/main/java/com/bookshelf26/BookShelf/controllers/BookController.java
+++ BookShelf/src/main/java/com/bookshelf26/BookShelf/controllers/BookController.java
@@ -1,13 +1,13 @@
 package com.bookshelf26.BookShelf.controllers;
 
 import com.bookshelf26.BookShelf.modals.Book;
+
 import org.springframework.ui.Model;
 import com.bookshelf26.BookShelf.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+
 
 @RequiredArgsConstructor
 @Controller("/")
@@ -15,9 +15,9 @@ public class BookController {
     private final BookService bookService;
 
 
-    @GetMapping()
+    @GetMapping("/")
     public String books(Model model){
-        model.addAttribute("books",bookService.listBooks());
+        model.addAttribute("books",bookService.list());
         return "books";
     }
     @GetMapping("/book/{id}")
@@ -26,12 +26,12 @@ public class BookController {
         return "book-info";
 
     }
-    @PatchMapping("/book/create")
+    @PostMapping("/book/create")
     public String createBook(Book book){
         bookService.saveBook(book);
         return "redirect:/";
     }
-    @PatchMapping("/book/delete/{id}")
+    @PostMapping("/book/delete/{id}")
     public String deleteBook(@PathVariable Long id){
         bookService.deleteBook(id);
         return "redirect:/";
